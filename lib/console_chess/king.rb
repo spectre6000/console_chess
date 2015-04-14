6afc093191bd44e3b9c317c7dd72a7f711737d88
spectre6000 <spectre6000@gmail.com>
2015-04-14 14:04:29 -0600

    refactored king movement logic

--- lib/console_chess/king.rb
+++ lib/console_chess/king.rb
@@ -13,14 +13,14 @@ module ConsoleChess
     end
 
     def populate_available_moves
-      @available_moves << "#{(@column.ord - 1).chr}#{@row.to_i + 1}" if @column.ord - 1 > 96 && @row.to_i + 1 < 9
-      @available_moves << "#{(@column.ord + 0).chr}#{@row.to_i + 1}" if @row.to_i + 1 < 9
-      @available_moves << "#{(@column.ord + 1).chr}#{@row.to_i + 1}" if @column.ord + 1 < 105 && @row.to_i + 1 < 9
-      @available_moves << "#{(@column.ord - 1).chr}#{@row.to_i + 0}" if @column.ord - 1 > 96
-      @available_moves << "#{(@column.ord + 1).chr}#{@row.to_i + 0}" if @column.ord + 1 < 105
-      @available_moves << "#{(@column.ord - 1).chr}#{@row.to_i - 1}" if @column.ord - 1 > 96 && @row.to_i - 1 > 0
-      @available_moves << "#{(@column.ord + 0).chr}#{@row.to_i - 1}" if @row.to_i - 1 > 0
-      @available_moves << "#{(@column.ord + 1).chr}#{@row.to_i - 1}" if @column.ord + 1 < 105 && @row.to_i - 1 > 0
+      x = [ @row.to_i + 1 , @row.to_i , @row.to_i - 1 ]
+      y = [ (@column.ord - 1) , @column.ord, (@column.ord + 1) ]
+      
+      x.each do |x|
+        y.each do |y|
+          @available_moves << "#{y.chr}#{x}" if x > 0 && x < 9 && y > 96 && y < 105 && "#{y.chr}#{x}" != @position
+        end
+      end
     end
 
   end

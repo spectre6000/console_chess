8df5745898826994d1bf45734a5bca6c072c41b2
spectre6000 <spectre6000@gmail.com>
2015-04-16 12:57:05 -0600

    maybe got it this time

--- spec/console_chess/bishop_spec.rb
+++ spec/console_chess/bishop_spec.rb
@@ -17,7 +17,7 @@ module ConsoleChess
     end
 
     it "starts with the correct collection of available moves" do
-      #changing it again
+      #this could be the final test
     end
   end
 end

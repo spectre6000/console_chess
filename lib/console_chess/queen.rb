70a60ecf63e8182c608d8b41d229d9d9133e8df6
spectre6000 <spectre6000@gmail.com>
2015-04-17 13:35:28 -0600

    queen logic built and pre-refactored as well

--- lib/console_chess/queen.rb
+++ lib/console_chess/queen.rb
@@ -7,5 +7,13 @@ module ConsoleChess
       @row == "8" ? @token = "q" : @token = "Q"
     end
 
+    def populate_available_moves
+      @available_moves = []
+
+      directions = [move_ul, move_dl, move_ur, move_dr, move_backward, move_forward, move_left, move_right]
+
+      directions.each {|x| @available_moves.concat(linear_movement(&x))}
+    end
+
   end
 end 
--- spec/console_chess/queen_spec.rb
+++ spec/console_chess/queen_spec.rb
@@ -3,22 +3,32 @@ require "spec_helper"
 module ConsoleChess
   describe Queen do
     let (:board) {Board.new}
-    let (:bqueen) {Queen.new("d", "8", "Black", board)}
     let (:wqueen) {Queen.new("d", "1", "White", board)}
-
+    let (:bqueen) {Queen.new("d", "8", "Black", board)}
+    let (:mqueen) {Queen.new("e", "4", "Black", board)}
 
     it "initializes with the correct 'color'" do
-      expect(bqueen.token).to eql("q")
       expect(wqueen.token).to eql("Q")
+      expect(bqueen.token).to eql("q")
     end
 
     it "initializes with the correct position" do
-      expect(bqueen.position).to eql("d8")
       expect(wqueen.position).to eql("d1")
+      expect(bqueen.position).to eql("d8")
     end
 
     it "starts with the correct collection of available moves" do
+      wqueen.populate_available_moves
+      bqueen.populate_available_moves
 
+      expect(wqueen.available_moves).to eql([])
+      expect(bqueen.available_moves).to eql([])
+    end
+
+    it "populates the correct collection of moves after start" do
+      mqueen.populate_available_moves
+
+      expect(mqueen.available_moves).to eql(["d3", "c2", "d5", "c6", "f3", "g2", "f5", "g6", "e5", "e6", "e3", "e2", "d4", "c4", "b4", "a4", "f4", "g4", "h4"])
     end
 
   end
